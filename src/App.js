1827aa52efbd79e0006f9d52908e3b4f4476fc62
shaswat97-crypto <shaswat97@gmail.com>
2023-05-16 17:34:01 +0530

    banner done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import Header from './Components/Header';
+import Banner from "./Components/Banner";
+import Header from "./Components/Header";
 
 function App() {
   return (
-   <Header/>
+    <>
+      <Header />
+      <Banner />
+    </>
   );
 }
 
---
+++ src/Components/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className="overlay">
+        </div>
+        <div className="textcont">
+            <h1>Computer Engineering</h1>
+            <p>142,765 Computer Engineers follow this</p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
