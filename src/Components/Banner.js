6c72b1a67e16295dcfb3d93b03d477834981e011
shaswat97-crypto <shaswat97@gmail.com>
2023-05-18 16:57:06 +0530

    completed

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -15,3 +15,160 @@ function Banner() {
 }
 
 export default Banner
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="cholder">
+        <div className="cimg">
+          <img src={img1} alt="" />
+        </div>
+        <div className="ctext">
+          <div className="ctype">
+            <div className="type-img" style={{ width: "105px" }}>
+              <img src={Education} alt="" />
+            </div>
+            <div className="cbody">
+              <h1>
+                Tax Benifits for Investment under National Pension Scheme
+                launched by Government
+              </h1>
+              <div className="dotscont">
+                <div className="dots">...</div>
+              </div>
+            </div>
+            <p>
+              I've worked in UX for the better part of a decade. From now on, I
+              plan to rei...
+            </p>
+            <div className="profilecont">
+              <div className="profile">
+                <div className="pimg">
+                  <img src={p1} alt="" />
+                </div>
+                <div className="name">Sarah West</div>
+              </div>
+              <div className="views">
+                <div className="eyeimg">
+                  <img src={eye} alt="" />
+                </div>
+                <div className="eyetext">1.4k views</div>
+              </div>
+              <div className="shareimg">
+                <img src={share} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="cholder">
+        <div className={clicked ? "menu view" : "menu"}>
+          <div>Edit</div>
+          <div>Report</div>
+          <div>Option</div>
+        </div>
+        <div className="cimg">
+          <img src={img2} alt="" />
+        </div>
+        <div className="ctext">
+          <div className="ctype">
+            <div className="type-img" style={{ width: "85px" }}>
+              <img src={Meetup} alt="" />
+            </div>
+            <div className="cbody">
+              <h1 style={{ height: "29px" }}>
+                Finance & Investment Elite Social Mixer @Lujiaui
+              </h1>
+              <div
+                className={clicked ? "dotscont active" : "dotscont"}
+                onClick={handleClick}
+              >
+                <div className="dots">...</div>
+              </div>
+            </div>
+            <div className="ahemcont">
+              <img src={ahmedabad} alt="" />
+            </div>
+            <div className="ahembtn">Visit Website</div>
+            <div className="profilecont">
+              <div className="profile">
+                <div className="pimg">
+                  <img src={p2} alt="" />
+                </div>
+                <div className="name">Ronal Jones</div>
+              </div>
+              <div className="views">
+                <div className="eyeimg">
+                  <img src={eye} alt="" />
+                </div>
+                <div className="eyetext">1.4k views</div>
+              </div>
+              <div className="shareimg">
+                <img src={share} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="cholder">
+        <div className="ctext">
+          <div className="ctype">
+            <div className="type-img" style={{ width: "53px" }}>
+              <img src={Job} alt="" />
+            </div>
+            <div className="cbody">
+              <h1 style={{ height: "29px" }}>Software Developer</h1>
+              <div className="dotscont">
+                <div className="dots">...</div>
+              </div>
+            </div>
+            <div className="ahemcont">
+              <img src={Ino} style={{ width: "399px" }} alt="" />
+            </div>
+            <div className="ahembtn" style={{ color: "#02B875" }}>
+              Apply on Timesjobs
+            </div>
+            <div className="profilecont">
+              <div className="profile">
+                <div className="pimg">
+                  <img src={p3} alt="" />
+                </div>
+                <div className="name">Joseph Gray</div>
+              </div>
+              <div className="views">
+                <div className="eyeimg">
+                  <img src={eye} alt="" />
+                </div>
+                <div className="eyetext">1.4k views</div>
+              </div>
+              <div className="shareimg">
+                <img src={share} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -17,10 +17,10 @@ import Job from "../asstes/Job.png";
 import Ino from "../asstes/Ino.png";
 
 function Content() {
-    const [clicked, setClicked] = useState(true);
-    const handleClick = () =>{
-        setClicked(prev=>!prev);
-    }
+  const [clicked, setClicked] = useState(true);
+  const handleClick = () => {
+    setClicked((prev) => !prev);
+  };
   return (
     <div className="content">
       <div className="cholder">
@@ -49,16 +49,19 @@ function Content() {
                 <div className="pimg">
                   <img src={p0} alt="" />
                 </div>
-                <div className="name">Sarthak Kamra</div>
-              </div>
-              <div className="views">
-                <div className="eyeimg">
-                  <img src={eye} alt="" />
+                <div className="epi">
+                  <div className="name">Sarthak Kamra</div>
+                  <div className="views">
+                    <div className="eyeimg">
+                      <img src={eye} alt="" />
+                    </div>
+                    <div className="eyetext">1.4k views</div>
+                  </div>
                 </div>
-                <div className="eyetext">1.4k views</div>
               </div>
               <div className="shareimg">
                 <img src={share} alt="" />
+                <div className="eyetext">Share</div>
               </div>
             </div>
           </div>
@@ -70,13 +73,13 @@ function Content() {
           <img src={img1} alt="" />
         </div>
         <div className="ctext">
-        <div className="ctype" >
-            <div className="type-img" style={{width:'105px'}}>
+          <div className="ctype">
+            <div className="type-img">
               <img src={Education} alt="" />
             </div>
             <div className="cbody">
               <h1>
-                Tax Benifits for Investment under National Pension Scheme
+              Tax Benifits for Investment under National Pension Scheme
                 launched by Government
               </h1>
               <div className="dotscont">
@@ -92,16 +95,19 @@ function Content() {
                 <div className="pimg">
                   <img src={p1} alt="" />
                 </div>
-                <div className="name">Sarah West</div>
-              </div>
-              <div className="views">
-                <div className="eyeimg">
-                  <img src={eye} alt="" />
+                <div className="epi">
+                  <div className="name">Sarah West</div>
+                  <div className="views">
+                    <div className="eyeimg">
+                      <img src={eye} alt="" />
+                    </div>
+                    <div className="eyetext">1.4k views</div>
+                  </div>
                 </div>
-                <div className="eyetext">1.4k views</div>
               </div>
               <div className="shareimg">
                 <img src={share} alt="" />
+                <div className="eyetext">Share</div>
               </div>
             </div>
           </div>
@@ -109,46 +115,51 @@ function Content() {
       </div>
 
       <div className="cholder">
-        <div className={clicked?"menu view":"menu"}>
-            <div>Edit</div>
-            <div>Report</div>
-            <div>Option</div>
+      <div className={clicked ? "menu view" : "menu"}>
+          <div>Edit</div>
+          <div>Report</div>
+          <div>Option</div>
         </div>
         <div className="cimg">
           <img src={img2} alt="" />
         </div>
         <div className="ctext">
-        <div className="ctype" >
-            <div className="type-img" style={{width:'85px'}}>
+          <div className="ctype">
+            <div className="type-img">
               <img src={Meetup} alt="" />
             </div>
             <div className="cbody">
-              <h1 style={{height:'29px'}}>Finance & Investment Elite Social Mixer @Lujiaui</h1>
-              <div className={clicked?"dotscont active":"dotscont"} onClick={handleClick}>
+              <h1>
+              Finance & Investment Elite Social Mixer @Lujiaui
+              </h1>
+              
+              <div className={clicked ? "dotscont active" : "dotscont"}
+                onClick={handleClick}>
                 <div className="dots">...</div>
               </div>
             </div>
             <div className="ahemcont">
-                <img src={ahmedabad} alt="" />
+              <img src={ahmedabad} alt="" />
             </div>
-            <div className="ahembtn">
-                Visit Website
-            </div>
+            <div className="ahembtn">Visit Website</div>
             <div className="profilecont">
               <div className="profile">
                 <div className="pimg">
                   <img src={p2} alt="" />
                 </div>
-                <div className="name">Ronal Jones</div>
-              </div>
-              <div className="views">
-                <div className="eyeimg">
-                  <img src={eye} alt="" />
+                <div className="epi">
+                  <div className="name">Ronal Jones</div>
+                  <div className="views">
+                    <div className="eyeimg">
+                      <img src={eye} alt="" />
+                    </div>
+                    <div className="eyetext">1.4k views</div>
+                  </div>
                 </div>
-                <div className="eyetext">1.4k views</div>
               </div>
               <div className="shareimg">
                 <img src={share} alt="" />
+                <div className="eyetext">Share</div>
               </div>
             </div>
           </div>
@@ -157,37 +168,40 @@ function Content() {
 
       <div className="cholder">
         <div className="ctext">
-          <div className="ctype" >
-            <div className="type-img" style={{width:'53px'}}>
+          <div className="ctype">
+            <div className="type-img">
               <img src={Job} alt="" />
             </div>
             <div className="cbody">
-              <h1 style={{height:'29px'}}>Software Developer</h1>
+            <h1 style={{ height: "29px" }}>Software Developer</h1>
               <div className="dotscont">
                 <div className="dots">...</div>
               </div>
             </div>
             <div className="ahemcont">
-                <img src={Ino} style={{width:'399px'}} alt="" />
+              <img src={Ino} style={{ width: "399px" }} alt="" />
             </div>
-            <div className="ahembtn" style={{color:'#02B875'}}>
-                Apply on Timesjobs
+            <div className="ahembtn" style={{ color: "#02B875" }}>
+              Apply on Timesjobs
             </div>
             <div className="profilecont">
               <div className="profile">
                 <div className="pimg">
                   <img src={p3} alt="" />
                 </div>
-                <div className="name">Joseph Gray</div>
-              </div>
-              <div className="views">
-                <div className="eyeimg">
-                  <img src={eye} alt="" />
+                <div className="epi">
+                  <div className="name">Joseph Gray</div>
+                  <div className="views">
+                    <div className="eyeimg">
+                      <img src={eye} alt="" />
+                    </div>
+                    <div className="eyetext">1.4k views</div>
+                  </div>
                 </div>
-                <div className="eyetext">1.4k views</div>
               </div>
               <div className="shareimg">
                 <img src={share} alt="" />
+                <div className="eyetext">Share</div>
               </div>
             </div>
           </div>
